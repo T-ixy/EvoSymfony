842110a962d0ca7344be1867e8f02add5205ed6f
T-ixy <nantenaina.tix@gmail.com>
2023-03-01 15:52:41 +0300

    finition de l'header

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
+use App\Repository\SanctionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(SanctionsRepository $sanctionRepo, CategoriesRepository $categoryRepo): Response
     {
+        $sanctions = $sanctionRepo->findAll();
+
+        $categories = $categoryRepo->findAll();
+
         return $this->render('home/index.html.twig', [
             'Page_title' => 'Accueil',
+            'sanctions'  => $sanctions,
+            'categories' => $categories
         ]);
     }
 }
