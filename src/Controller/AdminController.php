3ed59a61b1dc725b18faa6e6beb8136b87c41486
T-ixy <nantenaina.tix@gmail.com>
2023-03-14 14:50:20 +0300

    affichage des formations en admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Functions\Construct;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+class AdminController extends Construct
 {
+
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
+
+        $formations = $this->formationRepo->findAll();
+
         return $this->render('admin/index.html.twig', [
             'Page_title' => "Admin ~ Formations",
+            'formations' => $formations
+        ]);
+    }
+
+    #[Route('/admin/formation/add', name: 'app_admin_formation')]
+    public function formationAdd(): Response
+    {
+
+        return $this->render('admin/formationAdd.html.twig', [
+            'Page_title' => "Admin ~ Ajout de formation"
         ]);
     }
 
@@ -22,5 +38,21 @@ class AdminController extends AbstractController
         return $this->render('admin/profil.html.twig', [
             'Page_title' => "Admin ~ Profil",
         ]);
+    }
+
+    #[Route('/admin/delete/{data}/{id}', name: 'app_delete')]
+    public function delete(int $id, string $data, EntityManagerInterface $manager)
+    {
+        switch ($data) {
+            case 'formation':
+                $formation = $this->formationRepo->find($id);
+
+                $manager->remove($formation);
+                $manager->flush();
+
+                return $this->redirectToRoute('app_admin');
+                
+                break;
+        }
     }
 }
