5f5bc54ee61f7714e63c63f9c5d5304106c512f1
T-ixy <nantenaina.tix@gmail.com>
2023-04-07 11:03:45 +0300

    debug(entity):moddification de param de formation

--- migrations/Version20230407080211.php
+++ migrations/Version20230407080211.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230309081516 extends AbstractMigration
+final class Version20230407080211 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,12 @@ final class Version20230309081516 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, category VARCHAR(255) NOT NULL, icone_url VARCHAR(255) NOT NULL, color VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE formations (id INT AUTO_INCREMENT NOT NULL, sanction_id INT DEFAULT NULL, university_id INT NOT NULL, title VARCHAR(255) NOT NULL, generality LONGTEXT NOT NULL, prerequisite LONGTEXT DEFAULT NULL, purpose LONGTEXT NOT NULL, finality LONGTEXT DEFAULT NULL, contents LONGTEXT NOT NULL, prices INT DEFAULT NULL, duration VARCHAR(255) NOT NULL, priority TINYINT(1) NOT NULL, vignette_url VARCHAR(255) NOT NULL, INDEX IDX_4090213796E0C11A (sanction_id), INDEX IDX_40902137309D1878 (university_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE formations (id INT AUTO_INCREMENT NOT NULL, sanction_id INT DEFAULT NULL, university_id INT NOT NULL, title VARCHAR(255) NOT NULL, generality LONGTEXT NOT NULL, prerequisite LONGTEXT DEFAULT NULL, purpose LONGTEXT NOT NULL, finality LONGTEXT DEFAULT NULL, contents LONGTEXT NOT NULL, prices INT DEFAULT NULL, duration VARCHAR(255) DEFAULT NULL, priority TINYINT(1) NOT NULL, vignette_url VARCHAR(255) NOT NULL, INDEX IDX_4090213796E0C11A (sanction_id), INDEX IDX_40902137309D1878 (university_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE formations_categories (formations_id INT NOT NULL, categories_id INT NOT NULL, INDEX IDX_1B7E00B23BF5B0C2 (formations_id), INDEX IDX_1B7E00B2A21214B7 (categories_id), PRIMARY KEY(formations_id, categories_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, mail VARCHAR(255) NOT NULL, subject VARCHAR(255) DEFAULT NULL, message LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE sanctions (id INT AUTO_INCREMENT NOT NULL, sanction VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE universities (id INT AUTO_INCREMENT NOT NULL, university VARCHAR(255) NOT NULL, logo_url VARCHAR(255) NOT NULL, login_url VARCHAR(255) NOT NULL, site_url VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE formations ADD CONSTRAINT FK_4090213796E0C11A FOREIGN KEY (sanction_id) REFERENCES sanctions (id)');
         $this->addSql('ALTER TABLE formations ADD CONSTRAINT FK_40902137309D1878 FOREIGN KEY (university_id) REFERENCES universities (id)');
@@ -46,6 +47,7 @@ final class Version20230309081516 extends AbstractMigration
         $this->addSql('DROP TABLE message');
         $this->addSql('DROP TABLE sanctions');
         $this->addSql('DROP TABLE universities');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
